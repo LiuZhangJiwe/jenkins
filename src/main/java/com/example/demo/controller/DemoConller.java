e881246ed65bccff6cf370dae7126c3f988ae0a8
LiuHuan <919522511@qq.com>
2020-12-06 17:36:21 +0800

    测试jenkins

---
+++ src/main/java/com/example/demo/controller/DemoConller.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liuhuan
+ * @Date 2020/12/6 17:17
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoConller {
+    @RequestMapping("/test")
+    public String test(){
+        return "测试jenkins!";
+    }
+}
